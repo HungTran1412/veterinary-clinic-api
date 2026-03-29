a482f59fd1f3ab22d7e4b979569c063431b8b32e
Hung Tran Nguyen Minh <hug2k4123@gmail.com>
2026-03-29 15:46:56 +0700

    feat(api): thêm lấy danh sách email log nhưng chưa có api ((-

---
+++ VeterinaryClinic.Business/Business/EmailLogs/EmailLogModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeterinaryClinic.Business
+{
+    public class EmailLogBaseModel
+    {
+        [Required(ErrorMessage = "service.emailog-id.required")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "service.emailog-code.required")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "service.emailog-to-email.required")]
+        public string ToEmail { get; set; }
+        [Required(ErrorMessage = "service.emailog-subject.required")]
+        public string Subject { get; set; }
+        public string SentStatus { set; get; }
+        public string ErrorMessage { get; set; } 
+    }
+
+    public class EmailLogModel : EmailLogBaseModel
+    {
+        public string Body { set; get; }
+    }
+
+    public class EmailLogFilterModel : BaseQueryFilterModel
+    {
+        
+    }
+}

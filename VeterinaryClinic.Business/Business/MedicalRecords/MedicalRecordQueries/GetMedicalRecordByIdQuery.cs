0d27160c772fadb1ec052f80073964bc75ac5e3b
Hung Tran Nguyen Minh <hug2k4123@gmail.com>
2026-05-08 23:19:36 +0700

    feat(api): call stored procedure

--- VeterinaryClinic.Business/Business/MedicalRecords/MedicalRecordQueries/GetMedicalRecordByIdQuery.cs
+++ VeterinaryClinic.Business/Business/MedicalRecords/MedicalRecordQueries/GetMedicalRecordByIdQuery.cs
@@ -1,12 +1,10 @@
 ﻿using MediatR;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
-using VeterinaryClinic.Data;
 using VeterinaryClinic.Shared;
 
 namespace VeterinaryClinic.Business
 {
-    public class GetMedicalRecordByIdQuery : IRequest<MedicalRecordModel>
+    public class GetMedicalRecordByIdQuery : IRequest<MedicalInfoModel>
     {
         public int Id { get; }
 
@@ -15,31 +13,29 @@ namespace VeterinaryClinic.Business
             Id = id;
         }
 
-        public class Handler : IRequestHandler<GetMedicalRecordByIdQuery, MedicalRecordModel>
+        public class Handler : IRequestHandler<GetMedicalRecordByIdQuery, MedicalInfoModel>
         {
-            private readonly VeterinaryClinicReadDataContext _dataContext;
             private readonly IStringLocalizer<GetMedicalRecordByIdQuery> _localizer;
+            private readonly IVeterinaryClinicCallStoreHelper _callStoreHelper;
 
-            public Handler(VeterinaryClinicReadDataContext dataContext, IStringLocalizer<GetMedicalRecordByIdQuery> localizer)
+            public Handler(IStringLocalizer<GetMedicalRecordByIdQuery> localizer, IVeterinaryClinicCallStoreHelper callStoreHelper)
             {
-                _dataContext = dataContext;
                 _localizer = localizer;
+                _callStoreHelper = callStoreHelper;
             }
 
-            public async Task<MedicalRecordModel> Handle(GetMedicalRecordByIdQuery request, CancellationToken cancellationToken)
+            public async Task<MedicalInfoModel> Handle(GetMedicalRecordByIdQuery request, CancellationToken cancellationToken)
             {
-                var medicalRecord = await _dataContext.VcMedicalRecords
-                    .AsNoTracking()
-                    .FirstOrDefaultAsync(mr => mr.Id == request.Id, cancellationToken);
+                var dataTable = _callStoreHelper.CallStoreGetMedicalRecordByIdAsync(request.Id);
 
-                string cacheKey = MedicalRecordConstant.BuildCacheKey(request.Id.ToString());
-                
-                if (medicalRecord == null)
+                if (dataTable == null || dataTable.Rows.Count == 0)
                 {
                     throw new KeyNotFoundException(_localizer["medical_record.not_found"]);
                 }
 
-                return AutoMapperUtils.AutoMap<VcMedicalRecords, MedicalRecordModel>(medicalRecord);
+                var medicalInfo = dataTable.Rows[0].ToObject<MedicalInfoModel>();
+                
+                return await Task.FromResult(medicalInfo);
             }
         }
     }
